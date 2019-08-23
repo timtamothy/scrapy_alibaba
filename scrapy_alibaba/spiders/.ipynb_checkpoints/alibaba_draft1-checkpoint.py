3c61541d4f67722a1b747ce0d5836224e1a52ab6
Timothy Wang <stevetimothy@gmail.com>
2019-08-23 08:57:50 -0700

    Yield via items file

--- scrapy_alibaba/spiders/.ipynb_checkpoints/alibaba_draft1-checkpoint.py
+++ scrapy_alibaba/spiders/.ipynb_checkpoints/alibaba_draft1-checkpoint.py
@@ -9,39 +9,23 @@ from scrapy_alibaba.items import ScrapyAlibabaItem
 
 class AlibabaCrawlerSpider(scrapy.Spider):
     name = 'alibaba_debug'
-    #allowed_domains = ['alibaba.com']
-    #start_urls = ['https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText=headphones&viewtype=G&tab=']
-    
-    #search_text = "headphones"
-    
-    #def start_requests(self):
-    #Read keywords from keywords file and construct the search URL
-    # The meta is used to send our search text into the parser as metadata
-       # start_urls = 'https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText=headphones&viewtype=G&tab='
-       # search_text = "headphones"
-       # url = start_urls
-       # yield scrapy.Request(url, callback = self.parse, meta = {"search_text": search_text})
     
     def start_requests(self):
-        urls = ['https://www.alibaba.com/products/doors.html?spm=a2700.galleryofferlist.pagination.1.2669189flzhOGI&IndexArea=product_en&page='+str(i) for i in range(1, 101)]
+        urls = ['https://www.alibaba.com/products/doors.html?spm=a2700.galleryofferlist.pagination.1.2669189flzhOGI&IndexArea=product_en&page='+str(i) for i in range(1, 5)]
 
         for url in urls:
             yield scrapy.Request(url = url, callback = self.parse)
             
 
     def parse(self, response):
-        """Function to process alibaba search results page"""
-        #search_keyword = response.meta["search_text"]
         parser = scrapy.Selector(response)
         products = parser.xpath("//div[@class='item-main']")
     
-        #iterating over search results  
-        ##for product in products:
         #Defining the XPaths
         XPATH_PRODUCT_NAME = ".//div[@class='item-info']//h2[contains(@class,'title')]//a/@title"
             #XPATH_PRODUCT_PRICE =  ".//div[@class='item-info']//div[@class='price']/b/text()"
             #XPATH_PRODUCT_MIN_ORDER = ".//div[@class='item-info']//div[@class='min-order']/b/text()"
-            #XPATH_SELLER_YEARS = ".//div[@class='item-info']//div[@class='stitle']//div[contains(@class,'s-gold-supplier-year-icon')]//text()"
+        XPATH_SELLER_YEARS = ".//div[@class='item-info']//div[@class='stitle']//div[contains(@class,'s-gold-supplier-year-icon')]//text()"
         XPATH_SUPPLIER = ".//div[@class='item-info']//div[@class='stitle']//a/@title"
             #XPATH_SELLER_RESPONSE_RATE = ".//div[@class='item-info']//div[@class='sstitle']//div[@class='num']/i/text()"
             #XPATH_TRANSACTION_LEVEL = ".//div[@class='item-info']//div[@class='sstitle']//a[@class='diamond-level-group']//i[contains(@class,'diamond-level-one')]"
@@ -53,7 +37,7 @@ class AlibabaCrawlerSpider(scrapy.Spider):
         product_name = products.xpath(XPATH_PRODUCT_NAME).extract()
             #raw_product_price = products.xpath(XPATH_PRODUCT_PRICE).extract()
             #raw_minimum_order = products.xpath(XPATH_PRODUCT_MIN_ORDER).extract()
-            #raw_seller_years = products.xpath(XPATH_SELLER_YEARS).extract()
+        seller_years = products.xpath(XPATH_SELLER_YEARS).extract()
         supplier_name = products.xpath(XPATH_SUPPLIER).extract()
             #raw_seller_response_rate = products.xpath(XPATH_SELLER_RESPONSE_RATE).extract()
             #raw_transaction_level = products.xpath(XPATH_TRANSACTION_LEVEL).extract()
@@ -80,21 +64,8 @@ class AlibabaCrawlerSpider(scrapy.Spider):
             item = ScrapyAlibabaItem()
             item['product'] = product
             item['supplier'] = supplier
+            item['seller_years'] = seller_years
             yield item
-        
-        
-        #yield {
-            #'product_name':product_name,
-            #'product_price':product_price,
-            #'minimum_order':minimum_order,
-            #'seller_years_on_alibaba':seller_years_on_alibaba,
-            #'supplier_name':supplier_name,
-            #'seller_response_rate':seller_response_rate,
-            #'transaction_level':transaction_level,
-            #'product_link':product_link,
-            #'search_text':search_keyword
-            #}
-        
         
         
 
